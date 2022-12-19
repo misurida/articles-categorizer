9134e84f18058ea51015a167dd9ce2494a7708d7
Misurida <erwan.guyomarch@gmail.com>
2022-12-19 10:27:04 +0100

    Firebase config

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMbIjbXo5Bd6uyj-qQNkwPN7P89WJLDEA",
+  authDomain: "articles-categorizer.firebaseapp.com",
+  databaseURL: "https://articles-categorizer-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "articles-categorizer",
+  storageBucket: "articles-categorizer.appspot.com",
+  messagingSenderId: "1083336817132",
+  appId: "1:1083336817132:web:c41bd0031174ade073a8c9"
+};
